13ecf230bc34875eeedd728ab63e27a54646f5f5
Miguel <miguel.srmv@gmail.com>
2024-04-29 17:17:43 +0100

    Move left and move right

--- src/simulation/movements.c
+++ src/simulation/movements.c
@@ -6,7 +6,7 @@
 /*   By: mde-sa-- <mde-sa--@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 09:54:56 by mde-sa--          #+#    #+#             */
-/*   Updated: 2024/04/29 10:18:21 by mde-sa--         ###   ########.fr       */
+/*   Updated: 2024/04/29 17:16:42 by mde-sa--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,6 @@ void	move_foward(t_map_data *map_data, t_vector_data *vector_data)
 			+= round(vector_data->vector_dir_y * MOVE_SPEED);
 	printf("Position after movement:\n");
 	print_vector_data(vector_data);
-	printf("Pos X was added %f and Pos Y was added %f",
-		vector_data->vector_dir_x * MOVE_SPEED,
-		vector_data->vector_dir_y * MOVE_SPEED);
 }
 
 /// @brief Walks 1 MOVE_SPEED backwads
@@ -49,31 +46,66 @@ void	move_backwards(t_map_data *map_data, t_vector_data *vector_data)
 		vector_data->pos_y -= round(vector_data->vector_dir_y * MOVE_SPEED);
 	printf("Position after movement:\n");
 	print_vector_data(vector_data);
-	printf("Pos X was subtracted %f and Pos Y was subtracted %f",
-		vector_data->vector_dir_x * MOVE_SPEED,
-		vector_data->vector_dir_y * MOVE_SPEED);
 }
 
 /// @brief Walks 1 MOVE_SPEED left
+/// TODO: change directions depending on where one is facing
 void	move_left(t_map_data *map_data, t_vector_data *vector_data)
 {
+	double	old_dir_x;
+	double	old_dir_y;
+
+	old_dir_x = vector_data->vector_dir_x;
+	old_dir_y = vector_data->vector_dir_y;
+	vector_data->vector_dir_x = vector_data->vector_dir_x * cos(-M_PI / 2)
+		- vector_data->vector_dir_y * sin(-M_PI / 2);
+	vector_data->vector_dir_y = old_dir_x * sin(-M_PI / 2)
+		+ vector_data->vector_dir_y * cos(-M_PI / 2);
 	if ((map_data->map_tab
 			[(int)vector_data->pos_y]
-			[(int)(vector_data->pos_x + MOVE_SPEED)]) != '1')
-		vector_data->pos_x += MOVE_SPEED;
+			[(int)(vector_data->pos_x
+				+ vector_data->vector_dir_x * MOVE_SPEED)]) != '1')
+		vector_data->pos_x
+			+= round(vector_data->vector_dir_x * MOVE_SPEED);
+	if ((map_data->map_tab
+			[(int)(vector_data->pos_y
+				+ vector_data->vector_dir_y * MOVE_SPEED)]
+		[(int)(vector_data->pos_x)]) != '1')
+		vector_data->pos_y
+			+= round(vector_data->vector_dir_y * MOVE_SPEED);
+	vector_data->vector_dir_x = old_dir_x;
+	vector_data->vector_dir_y = old_dir_y;
 	printf("Position after movement:\n");
 	print_vector_data(vector_data);
-	printf("Pos X was added %d", MOVE_SPEED);
 }
 
 /// @brief Walks 1 MOVE_SPEED right
+/// TODO: change directions depending on where one is facing
 void	move_right(t_map_data *map_data, t_vector_data *vector_data)
 {
+	double	old_dir_x;
+	double	old_dir_y;
+
+	old_dir_x = vector_data->vector_dir_x;
+	old_dir_y = vector_data->vector_dir_y;
+	vector_data->vector_dir_x = vector_data->vector_dir_x * cos(M_PI / 2)
+		- vector_data->vector_dir_y * sin(M_PI / 2);
+	vector_data->vector_dir_y = old_dir_x * sin(M_PI / 2)
+		+ vector_data->vector_dir_y * cos(M_PI / 2);
 	if ((map_data->map_tab
 			[(int)vector_data->pos_y]
-			[(int)(vector_data->pos_x - MOVE_SPEED)]) != '1')
-		vector_data->pos_x -= MOVE_SPEED;
+			[(int)(vector_data->pos_x
+				+ vector_data->vector_dir_x * MOVE_SPEED)]) != '1')
+		vector_data->pos_x
+			+= round(vector_data->vector_dir_x * MOVE_SPEED);
+	if ((map_data->map_tab
+			[(int)(vector_data->pos_y
+				+ vector_data->vector_dir_y * MOVE_SPEED)]
+		[(int)(vector_data->pos_x)]) != '1')
+		vector_data->pos_y
+			+= round(vector_data->vector_dir_y * MOVE_SPEED);
+	vector_data->vector_dir_x = old_dir_x;
+	vector_data->vector_dir_y = old_dir_y;
 	printf("Position after movement:\n");
 	print_vector_data(vector_data);
-	printf("Pos X was subtracted %d", MOVE_SPEED);
 }
