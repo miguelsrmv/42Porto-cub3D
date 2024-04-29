2302f9fc0a3ded063b7aa22116650122bd291bbf
Miguel <miguel.srmv@gmail.com>
2024-04-29 18:16:15 +0100

    Faster move left and right

--- src/simulation/movements.c
+++ src/simulation/movements.c
@@ -6,7 +6,7 @@
 /*   By: mde-sa-- <mde-sa--@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 09:54:56 by mde-sa--          #+#    #+#             */
-/*   Updated: 2024/04/29 17:16:42 by mde-sa--         ###   ########.fr       */
+/*   Updated: 2024/04/29 18:15:25 by mde-sa--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,63 +49,41 @@ void	move_backwards(t_map_data *map_data, t_vector_data *vector_data)
 }
 
 /// @brief Walks 1 MOVE_SPEED left
-/// TODO: change directions depending on where one is facing
 void	move_left(t_map_data *map_data, t_vector_data *vector_data)
 {
-	double	old_dir_x;
-	double	old_dir_y;
+	double	perpendicular_x;
+	double	perpendicular_y;
 
-	old_dir_x = vector_data->vector_dir_x;
-	old_dir_y = vector_data->vector_dir_y;
-	vector_data->vector_dir_x = vector_data->vector_dir_x * cos(-M_PI / 2)
-		- vector_data->vector_dir_y * sin(-M_PI / 2);
-	vector_data->vector_dir_y = old_dir_x * sin(-M_PI / 2)
-		+ vector_data->vector_dir_y * cos(-M_PI / 2);
-	if ((map_data->map_tab
-			[(int)vector_data->pos_y]
-			[(int)(vector_data->pos_x
-				+ vector_data->vector_dir_x * MOVE_SPEED)]) != '1')
-		vector_data->pos_x
-			+= round(vector_data->vector_dir_x * MOVE_SPEED);
-	if ((map_data->map_tab
-			[(int)(vector_data->pos_y
-				+ vector_data->vector_dir_y * MOVE_SPEED)]
-		[(int)(vector_data->pos_x)]) != '1')
-		vector_data->pos_y
-			+= round(vector_data->vector_dir_y * MOVE_SPEED);
-	vector_data->vector_dir_x = old_dir_x;
-	vector_data->vector_dir_y = old_dir_y;
+	perpendicular_x = vector_data->vector_dir_y;
+	perpendicular_y = -vector_data->vector_dir_x;
+	if (map_data->map_tab
+		[(int)(vector_data->pos_y + perpendicular_y * MOVE_SPEED)]
+		[(int)(vector_data->pos_x)] != '1')
+		vector_data->pos_y += round(perpendicular_y * MOVE_SPEED);
+	if (map_data->map_tab
+		[(int)(vector_data->pos_y)]
+		[(int)(vector_data->pos_x + perpendicular_x * MOVE_SPEED)] != '1')
+		vector_data->pos_x += round(perpendicular_x * MOVE_SPEED);
 	printf("Position after movement:\n");
 	print_vector_data(vector_data);
 }
 
 /// @brief Walks 1 MOVE_SPEED right
-/// TODO: change directions depending on where one is facing
 void	move_right(t_map_data *map_data, t_vector_data *vector_data)
 {
-	double	old_dir_x;
-	double	old_dir_y;
+	double	perpendicular_x;
+	double	perpendicular_y;
 
-	old_dir_x = vector_data->vector_dir_x;
-	old_dir_y = vector_data->vector_dir_y;
-	vector_data->vector_dir_x = vector_data->vector_dir_x * cos(M_PI / 2)
-		- vector_data->vector_dir_y * sin(M_PI / 2);
-	vector_data->vector_dir_y = old_dir_x * sin(M_PI / 2)
-		+ vector_data->vector_dir_y * cos(M_PI / 2);
-	if ((map_data->map_tab
-			[(int)vector_data->pos_y]
-			[(int)(vector_data->pos_x
-				+ vector_data->vector_dir_x * MOVE_SPEED)]) != '1')
-		vector_data->pos_x
-			+= round(vector_data->vector_dir_x * MOVE_SPEED);
-	if ((map_data->map_tab
-			[(int)(vector_data->pos_y
-				+ vector_data->vector_dir_y * MOVE_SPEED)]
-		[(int)(vector_data->pos_x)]) != '1')
-		vector_data->pos_y
-			+= round(vector_data->vector_dir_y * MOVE_SPEED);
-	vector_data->vector_dir_x = old_dir_x;
-	vector_data->vector_dir_y = old_dir_y;
+	perpendicular_x = vector_data->vector_dir_y;
+	perpendicular_y = -vector_data->vector_dir_x;
+	if (map_data->map_tab
+		[(int)(vector_data->pos_y - perpendicular_y * MOVE_SPEED)]
+		[(int)(vector_data->pos_x)] != '1')
+		vector_data->pos_y -= round(perpendicular_y * MOVE_SPEED);
+	if (map_data->map_tab
+		[(int)(vector_data->pos_y)]
+		[(int)(vector_data->pos_x - perpendicular_x * MOVE_SPEED)] != '1')
+		vector_data->pos_x -= round(perpendicular_x * MOVE_SPEED);
 	printf("Position after movement:\n");
 	print_vector_data(vector_data);
 }
